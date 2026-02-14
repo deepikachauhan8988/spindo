0f4b3cb6bcf7967bc3ea8bb114c7e50c9f21dca3
Deepika <deepikachauhan8988@gmail.com>
2026-02-14 14:22:05 +0530

    Navbar fixed

--- src/componets/navbar/NavBar.js
+++ src/componets/navbar/NavBar.js
@@ -13,7 +13,7 @@ function NavBar() {
     <>
       {/* Top bar with contact info and social links */}
       <div className="top-bar bg-dark text-white py-2 d-none d-lg-block">
-        <Container className="d-flex justify-content-between align-items-center">
+        <div className="d-flex justify-content-between align-items-center nav-p">
           <div className="contact-info">
             <span className="me-3"><FaEnvelope /> contact@example.com</span>
             <span><FaPhone /> +1 (123) 456-7890</span>
@@ -24,24 +24,26 @@ function NavBar() {
             <Link to="#" className="text-white me-3"><FaLinkedin /></Link>
             <Link to="#" className="text-white"><FaInstagram /></Link>
           </div>
-        </Container>
+        </div>
       </div>
       
       {/* Main navigation */}
       <Navbar expand="lg" className="bg-body-tertiary sticky-top">
-        <Container>
+       
           <Navbar.Brand href="#home">
             <div className="d-flex align-items-center">
-             <img src={Logo} alt="Spindo Logo" className="spi-logo img-fluid me-2" />
+            <Link to="/"> <img src={Logo} alt="Spindo Logo" className="spi-logo img-fluid me-2" /></Link>
           
              
             </div>
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
+         
           <Navbar.Collapse id="basic-navbar-nav">
             {/* Left side navigation items */}
+              <Container>
             <Nav className="me-auto">
-              <Nav.Link href="#home">HOME</Nav.Link>
+              <Nav.Link href="/">HOME</Nav.Link>
               <Nav.Link href="#about">ABOUT</Nav.Link>
               <NavDropdown title="SERVICES" id="basic-nav-dropdown">
                 <NavDropdown.Item href="#plumber">PLUMBER</NavDropdown.Item>
@@ -56,7 +58,7 @@ function NavBar() {
               <Nav.Link href="#payment">Payment</Nav.Link>
               <Nav.Link href="#book">Book Services</Nav.Link>
             </Nav>
-            
+            </Container>
             {/* Right side - Only Register and Login buttons */}
             <Nav className="ms-auto">
               <Button variant="outline-primary" className="me-2" href="#register">
@@ -75,7 +77,7 @@ function NavBar() {
               <Nav.Link href="tel:+11234567890"><FaPhone /> Phone</Nav.Link>
             </Nav>
           </Navbar.Collapse>
-        </Container>
+     
       </Navbar>
     </>
   );
--- src/componets/pages/Home.js
+++ src/componets/pages/Home.js
@@ -1,9 +1,8 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 
 function Home() {
   return (
-   <Link to="/AdminDashBoard"> <div>Home</div></Link>
+    <div>Home</div>
   )
 }
 
